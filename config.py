bfd42d4b97a7dd974ed527717f7375254e6c267a
neo1989 <neosu89@gmail.com>
2013-10-11 15:58:42 +0800

    conf

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ HOSTS = ['test@127.0.0.1']
 PASSWORD = '123456'
 
 
+PROJECT = 'test'
 
 
 #EOF
 #加载服务器配置
 extend = os.path.join(os.path.abspath(os.path.dirname(__file__)),'conf_extend.py')
 if os.path.exists(extend):
-        from conf_extend import *
+    from conf_extend import *
